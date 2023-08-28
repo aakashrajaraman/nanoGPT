44726abcfcfe090c7eb240c8c6faf2a909c3e96f
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-08-28 18:17:30 +0530

    residual connections

--- bigram.py
+++ bigram.py
@@ -6,10 +6,12 @@ import torch.nn.functional as F
 
 
 batch_size=32
-block_size = 10
+block_size = 8
 torch.manual_seed(0)
 learning_rate = 1e-3
-eval_interval = 300
+eval_interval = 500
+eval_iters = 200
+n_embd = 32
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -67,19 +69,84 @@ def loss_eval():
 
 #
 xb,yb = get_batch('train')
-print(xb.shape,'\n',xb)
-print(yb.shape,'\n',yb)
 
 
 #
+class Head(nn.Module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias = False)
+        self.query = nn.Linear(n_embd, head_size, bias = False)
+        self.value = nn.Linear(n_embd, head_size, bias = False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+    def forward(self, x):
+        B, T, C = x.shape
+        key = self.key(x)
+        query = self.query(x)
+        wei = query @ key.transpose(-2, -1)* C**(-0.5)
+        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        value = self.value(x)
+        out = wei @ value
+        return out
+
+class multiHead(nn.Module):
+    def __init__(self, n_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(n_heads)])
+        self.proj = nn.Linear(n_embd, n_embd) #transforms the heads into a single tensor to be outputted
+    def forward(self, x):
+        out =  torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        return out
+
+class FFN(nn.Module):
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+            nn.Linear(n_embd, n_embd),#adding a layer for residuals
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Block(nn.Module):
+    """ Transformer block: communication followed by computation """
+
+    def __init__(self, n_embd, n_head):
+        # n_embd: embedding dimension, n_head: the number of heads we'd like
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = multiHead(n_head, head_size)
+        self.ffwd = FFN(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
+        return x
+
+
+
+
 
 class Bigram(nn.Module):
     def __init__(self,  vocab):
         super().__init__()
-        self.token_embedding_table= nn.Embedding(vocab, vocab)
+        self.token_embedding_table= nn.Embedding(vocab, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)#hold positional value of each token
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=4) for _ in range(3)])
+        self.lm_head = nn.Linear(n_embd, vocab)#creates an intermediary layer to convert token embeddings to the logits
+        
+
     def forward(self, idx, targets=None):
-        logits= self.token_embedding_table(idx)#creates a [batch, chunk, vocab] tensor
-            #logits represent the next scores for each embedding
+        B, T = idx.shape
+        tok_embd= self.token_embedding_table(idx)#creates a [batch, chunk, n_embd] tensor
+        #logits represent the next scores for each embedding
+        position_embedding = self.position_embedding_table(torch.arange(T, device=device))#creates a [chunk, n_embd] tensor
+        x = tok_embd + position_embedding#adds the positional embedding to the token embedding
+        x = self.blocks(x)
+        logits = self.lm_head(x)#creates a [batch, chunk, vocab_size] tensor
        
         
         if targets is None:
@@ -94,7 +161,8 @@ class Bigram(nn.Module):
         #idx is the current token
         for _ in range(max):
             #get predictions
-            logits, loss = self(idx)
+            idx_c = idx[:,-block_size:]# crop to block size
+            logits, loss = self(idx_c)
             logits = logits[:,-1,:]#get the timestep to form (B,C) tensor
             probs= F.softmax(logits, dim=-1)
             next = torch.multinomial(probs, num_samples=1)
@@ -103,7 +171,6 @@ class Bigram(nn.Module):
 model = Bigram(vocab)
 m = model.to(device)
 logits, loss = m(xb, yb)
-print(logits.shape)
 
 
 #
@@ -116,7 +183,7 @@ optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
 
 #
 
-for i in range(10000):
+for i in range(5000):
 
     if i%eval_interval == 0:
         loss = loss_eval()
@@ -131,7 +198,7 @@ for i in range(10000):
 
 #
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, 100)[0].tolist()))
+print(decode(m.generate(context, 200)[0].tolist()))
 
 #
 
